a807bc25f9590f887cd4acce18d22fd7fcae2b17
jatinkumar30 <bhanurawal.30@gmail.com>
2024-04-13 23:40:28 +0530

    updated

---
+++ src/components/hero/hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./hero.css";
+const Hero = () => {
+    return (
+        <div className="hero">
+          <h5>Hi, my name is</h5>
+          <h1>Jatin Kumar</h1>
+          <p>
+            I'm a budding developer specializing in building optimal web applications. I am also a Black-belt (not flexing but true 😜). I always have been interested in latest technologies and geeky stuffs. Currenly I'm looking for opportunities, you can <span>Contact</span> me.
+          </p>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,85 @@
+import React, {useEffect, useState} from "react";
+import "./navbar.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+    const [openMenu, setOpenMenu] =  useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
+    const handleNavigationClick = (event , sectionId) => {
+        event.preventDefault();
+        const element = document.getElementById(sectionId);
+        if (element) {
+            element.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+            });
+        }
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+          setOpenMenu(false); // Close the menu when scrolling
+        };
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+
+      return (
+        <div className="nav">
+            <ol className="nav-right">
+                <li>
+                    <a
+                    className="navbar-link" 
+                    onClick={(e) => handleNavigationClick(e, "about")}
+                    href="">About</a>
+                </li>
+                <li>
+                    <a
+                    className="navbar-link" 
+                    onClick={(e) => handleNavigationClick(e, "skills")}
+                    href="">skills</a>
+                </li>
+                <li>
+                    <a
+                    className="navbar-link" 
+                    onClick={(e) => handleNavigationClick(e, "project")}
+                    href="">project</a>
+                </li>
+                <li>
+                    <a
+                    className="navbar-link" 
+                    onClick={(e) => handleNavigationClick(e, "contact")}
+                    href="">contact</a>
+                </li>
+                <a href="/resume" className="resume-link">resume</a>
+            </ol>
+
+            <div className="mobile-navbar-btn">
+        <FontAwesomeIcon
+          icon={faBars}
+          name="menu-outline"
+          className="mobile-navbar-icon menu-outline"
+          onClick={toggleMenu}
+        />
+        <FontAwesomeIcon
+          icon={faTimes}
+          name="close-outline"
+          className="close-outline mobile-navbar-icon"
+          onClick={toggleMenu}
+        />
+      </div>
+
+        </div>
+      )
+};
+
+export default Navbar;
